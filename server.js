c96fd96d20379c1e592596e3d56b8c4d6c7962f3
mohammad <miserable.farghaly93@gmail.com>
2020-08-27 09:47:03 +0200

    new3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 // Provide access to node_modules folder
-app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+app.use('/scripts', express.static(`/node_modules/`));
 
 // Redirect all traffic to index.html
 app.use((req, res) => res.sendFile(`${__dirname}/public/index.html`));
